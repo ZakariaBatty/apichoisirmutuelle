0ee72a8964f5f8b847f4697d846c0b3afe9615c7
ZakariaBAtty <zbatty0912@gmail.com>
2022-02-25 11:47:47 +0100

    change

--- server.js
+++ server.js
@@ -31,48 +31,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //@ USE ROUTER
-app.post('/create-pdf', (req, res) => {
+app.post('/create-pdf', cors(), (req, res) => {
   pdf.create(pdfTemplate(req.body), {}).toFile('mutuelle.pdf', err => {
     if (err) {
       console.log(err);
     } else {
-      let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        service: 'gmail',
-        auth: {
-          user: process.env.email,
-          pass: process.env.password_mail,
-        },
-      });
-      //   Hoodzpronos1@gmail.com
-      let mailOptions = {
-        from: process.env.email,
-        to: 'zbatty45@gmail.com',
-        subject: 'CHOISIR MUTUELLE',
-        email: req.body.email,
-        name: req.body.nom,
-        attachments: [
-          {
-            filename: `${req.body.nom}${req.body.prenom}.pdf`,
-            path: path.join(__dirname, './mutuelle.pdf'), // <= Here
-            contentType: 'application/pdf',
-          },
-        ],
-      };
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error.message);
-          return res.status(500).json({
-            error:
-              "Répétez encore et encore, une erreur s'est produite dans le fonctionnement",
-          });
-        } else {
-          res.status(200).json({ message: 'E-mail envoyé avec succès' });
-          console.log('E-mail envoyé avec succès');
-        }
-      });
+      res.status(200).json({ message: 'E-mail envoyé avec succès' });
+      console.log('E-mail envoyé avec succès');
     }
   });
 });
