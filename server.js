850073de7d40a027222daa29a52a84ceea4fbe43
ZakariaBAtty <zbatty0912@gmail.com>
2022-03-07 16:17:09 +0100

    chnage

--- server.js
+++ server.js
@@ -35,61 +35,49 @@ app.post('/', (req, res) => {
 });
 
 const createPdf = (req, res) => {
-  pdf
-    .create(pdfTemplate(req.body), {})
-    .toFile('mutuelle.pdf', err => {
-      if (err) {
-        console.log('not working');
-        return console.log(err);
-      } else {
-        const transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: 587,
-          secure: false,
-          service: 'gmail',
-          auth: {
-            user: process.env.email,
-            pass: process.env.password_mail,
-          },
-        });
-        //   Hoodzpronos1@gmail.com
-        const mailOptions = {
-          from: 'CHOISIR MUTUELLE',
-          to: 'zbatty1297@gmail.com',
-          subject: 'CHOISIR MUTUELLE',
-          email: req.body.email,
-          name: req.body.nom,
-          attachments: [
-            {
-              filename: `${req.body.nom}${req.body.prenom}.pdf`,
-              path: path.join(__dirname, './mutuelle.pdf'), // <= Here
-              contentType: 'application/pdf',
-            },
-          ],
-        };
-        transporter.sendMail(mailOptions, function (error, info) {
-          if (error) {
-            console.log(error.message);
-            return res.status(500).json({ message: 'Not workin' });
-          } else {
-            console.log('E-mail envoyé avec succès');
-            return res
-              .status(200)
-              .json({ message: 'E-mail envoyé avec succès from email' });
-          }
-        });
-      }
-    })
-    .then(res => {
-      return new Promise(resolve => {
-        if (!res) resolve('fail');
-        console.log('work good');
+  pdf.create(pdfTemplate(req.body), {}).toFile('mutuelle.pdf', err => {
+    if (err) {
+      console.log('not working');
+      return console.log(err);
+    } else {
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        service: 'gmail',
+        auth: {
+          user: process.env.email,
+          pass: process.env.password_mail,
+        },
       });
-    })
-    .catch(err => {
-      console.log(err);
-      console.log('work bad');
-    });
+      //   Hoodzpronos1@gmail.com
+      const mailOptions = {
+        from: 'CHOISIR MUTUELLE',
+        to: 'zbatty1297@gmail.com',
+        subject: 'CHOISIR MUTUELLE',
+        email: req.body.email,
+        name: req.body.nom,
+        attachments: [
+          {
+            filename: `${req.body.nom}${req.body.prenom}.pdf`,
+            path: path.join(__dirname, './mutuelle.pdf'), // <= Here
+            contentType: 'application/pdf',
+          },
+        ],
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error.message);
+          return res.status(500).json({ message: 'Not workin' });
+        } else {
+          console.log('E-mail envoyé avec succès');
+          return res
+            .status(200)
+            .json({ message: 'E-mail envoyé avec succès from email' });
+        }
+      });
+    }
+  });
 };
 
 //@ USE ROUTER
