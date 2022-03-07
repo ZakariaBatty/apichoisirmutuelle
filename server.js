1e389706f8d205f85d33b828f84eac31a60c7c61
ZakariaBAtty <zbatty0912@gmail.com>
2022-03-07 20:24:55 +0100

    testig

--- server.js
+++ server.js
@@ -34,47 +34,50 @@ app.post('/', (req, res) => {
   console.log('E-mail envoyé avec succès');
 });
 
-const createPdf = (req, res) => {
-  pdf.create(pdfTemplate(req.body), {}).toFile('mutuelle.pdf', err => {
-    if (err) {
-      console.log('not working');
-      return console.log(err);
+const sendMail = (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    service: 'gmail',
+    auth: {
+      user: process.env.email,
+      pass: process.env.password_mail,
+    },
+  });
+  const mailOptions = {
+    from: 'CHOISIR MUTUELLE',
+    to: 'zbatty1297@gmail.com',
+    subject: 'CHOISIR MUTUELLE',
+    email: req.body.email,
+    name: req.body.nom,
+    attachments: [
+      {
+        filename: `${req.body.nom}${req.body.prenom}.pdf`,
+        path: path.join(__dirname, './mutuelle.pdf'), // <= Here
+        contentType: 'application/pdf',
+      },
+    ],
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error.message);
+      return res.status(500).json({ message: 'Not workin' });
     } else {
-      const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        service: 'gmail',
-        auth: {
-          user: process.env.email,
-          pass: process.env.password_mail,
-        },
-      });
-      const mailOptions = {
-        from: 'CHOISIR MUTUELLE',
-        to: 'zbatty1297@gmail.com',
-        subject: 'CHOISIR MUTUELLE',
-        email: req.body.email,
-        name: req.body.nom,
-        attachments: [
-          {
-            filename: `${req.body.nom}${req.body.prenom}.pdf`,
-            path: path.join(__dirname, './mutuelle.pdf'), // <= Here
-            contentType: 'application/pdf',
-          },
-        ],
-      };
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error.message);
-          return res.status(500).json({ message: 'Not workin' });
-        } else {
-          console.log('E-mail envoyé avec succès');
-          return res
-            .status(200)
-            .json({ message: 'E-mail envoyé avec succès from email' });
-        }
-      });
+      console.log('E-mail envoyé avec succès');
+      return res
+        .status(200)
+        .json({ message: 'E-mail envoyé avec succès from email' });
+    }
+  });
+};
+
+const createPdf = async (req, res) => {
+  pdf.create(pdfTemplate(req.body), {}).toFile('mutuelle.pdf', err => {
+    try {
+      if (!err) sendMail(req, res);
+    } catch (error) {
+      return console.log(err);
     }
   });
 };
